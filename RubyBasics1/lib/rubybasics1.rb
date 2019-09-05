584548dbf9e40162b08018f037b17d78c8581de3
Richard Scott <rscott32@uncc.edu>
2019-09-05 01:23:25 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,24 @@
+# Lab 1
+# Part I
+def sum arr
+  add = 0
+  if arr.length == 0
+    add = 0
+  else
+    arr.each do |x|
+      add += x
+    end
+  end
+  return add
+end
+
+# Part II
+def max_2_sum arr
+  
+end
+
+# Part III
+def sum_to_n? arr, n
+  
+    
+end
